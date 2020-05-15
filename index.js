eccacfd14e3f3d2e00e93e37cfa6f02e88327e09
Darkounet <thibaultjanuel21@gmail.com>
2020-05-15 16:49:30 +0200

    OOPS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const fetch = require("node-fetch");
 const translator = require("translate");
 
-const { TOKEN } = require("credentials.json");
+const { TOKEN } = require("./credentials.json");
 
 const client = new Discord.Client();
 
